810eb87bf0830e5f0eb4c8dd41c9f73a9aa461d7
EstineClasen <estineclasen@gmail.com>
2023-03-29 23:28:10 +0200

    Multiprocessing and async simulate flow

--- EC_inference_pipeline.py
+++ EC_inference_pipeline.py
@@ -14,7 +14,7 @@ async def predict_spec_sliding_window():
 
 async def process_batch(input):
     print(f'process_batch function in process {os.getpid()} with args {input}')
-    # Need to split into chunks and can run each chunk concurrently -> I think async for?
+    # Need to split batch into chunks and can run each chunk concurrently -> I think use async for loop?
     await generate_spectrogram_of_chunk()      # simulates generating spectrogram
     await predict_spec_sliding_window()         # simulates prediction
     return input
@@ -23,7 +23,7 @@ async def process_batch(input):
 async def main(in_args):
     print(f'argument passed to async main {in_args}')
     print(f'number of available cores = {os.cpu_count()}')
-    # Use input arguments for dataloader
+    # Use input arguments for dataloader to get raw audio batches
     dataloader = [(1, 2), (3, 4), (5, 6), (7, 8), (9, 10), (11, 12), (13, 14), (15, 16), (17, 18), (19, 20), (21, 22),
                   (23, 24), (25, 26), (27, 28), (29, 30), (31, 32), (33, 34), (35, 36), (37, 38), (39, 40), (41, 42),
                   (43, 44), (45, 46), (47, 48)]
