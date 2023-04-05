5162126193185bec1cb4847172642fafaed9e3d3
EstineClasen <estineclasen@gmail.com>
2023-04-05 12:13:03 +0200

    Async gen for each processor chunk integrated. Still need to read entire file upfront.

--- EC_inference_pipeline.py
+++ EC_inference_pipeline.py
@@ -1,37 +1,83 @@
 import os
 import asyncio
 from aiomultiprocess import Pool
-import miniaudio
+# import multiprocessing as mp
+# from multiprocessing import Pool, cpu_count
+import sys
+import sounddevice as sd
+import soundfile as sf
+from math import ceil
 
+AUDIO_FILE_PATH = "C:\\Users\\estin\\PycharmProjects\\ElephantsEfficientProcessingTeam\\finding_manny\\input_data\\testmp3.mp3"
+
+BLOCK_SIZE = 2048
+current_frame = 0
 
 async def generate_spectrogram_of_chunk(chunkID, chunk):
     print(f'generate_spectrogram_of_chunk in {os.getpid()} with chunk {chunkID}')
-    await asyncio.sleep(2)      # simulates generating spectrogram
+    await asyncio.sleep(1)      # simulates generating spectrogram
     print(f'generate_spectrogram_of_chunk in {os.getpid()} woke up with chunk {chunkID}')
 
 
 async def predict_spec_sliding_window(chunkID, chunk):
     print(f'predict_spec_sliding_window in {os.getpid()} with chunk {chunkID}')
-    await asyncio.sleep(2)      # simulates prediction
+    await asyncio.sleep(1)      # simulates prediction
     print(f'predict_spec_sliding_window in {os.getpid()} woke up with chunk {chunkID}')
 
-async def process_batch(waveform_generator):
-    print(f'process_batch function in process {os.getpid()} with args {input}')
-    # Need to split batch into chunks and can run each chunk concurrently -> I think use async for loop?
+async def process_batch(processor_chunk):
+    # print(f'process_batch function in process {os.getpid()} with args {input}')
+    # Need to split processor_chunk into applicable chunks and can run each chunk concurrently -> I think use async for loop?
 
     i = 0
-    async for waveform in waveform_generator:
-        # do something with the waveform....
-        print(f'{i}th waveform size = {len(waveform)}')
+    async for outdata, status in data_stream_generator(processor_chunk):
+        print(f'processing {i}th chunk in data_stream_generatorsize = {len(outdata)}')
+        await generate_spectrogram_of_chunk(i, outdata)  # simulates generating spectrogram
+        await predict_spec_sliding_window(i, outdata)  # simulates prediction
         i += 1
-        await generate_spectrogram_of_chunk(i, waveform)  # simulates generating spectrogram
-        await predict_spec_sliding_window(i, waveform)  # simulates prediction
 
-    return input
+    return i
 
 
-async def main(in_args):
-    print(f'argument passed to async main {in_args}')
+async def data_stream_generator(indata_processor_chunk, blocksize=BLOCK_SIZE, *, channels=1, dtype='float32',
+                           pre_fill_blocks=10, **kwargs):
+    """Generator that yields blocks of output data from larger chunk passed (from wav file)
+     as NumPy arrays. The output blocks are uninitialized and have to be filled with
+        appropriate audio signals.
+        """
+    assert blocksize != 0
+    q_out = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+    fs = 48000
+    def callback(indata, frames, time, status):
+        global current_frame
+        if status:
+            print(status)
+        chunksize = min(len(indata_processor_chunk) - current_frame, frames)
+        indata[:chunksize] = indata_processor_chunk[current_frame:current_frame + chunksize]
+        if chunksize < frames:
+            indata[chunksize:] = 0
+            raise sd.CallbackStop()
+        current_frame += chunksize
+        loop.call_soon_threadsafe(q_out.put_nowait, (indata.copy(), status))
+        print(f'outputstream_generator callback executed with frames {frames}')
+        # outdata[:] = q_out.get_nowait()
+
+    stream = sd.OutputStream(samplerate=fs, device=sd.default.device, channels=indata_processor_chunk.shape[1], callback=callback)
+    with stream:
+        while True:
+            outdata, status = await q_out.get()
+            yield outdata, status
+            #q_out.put_nowait(outdata)
+            if q_out.empty():
+                #loop.stop()
+                #loop.close()   # ???
+                stream.stop()  # ???
+                break
+
+
+
+async def main(datadir):
+    print(f'argument passed to async main {datadir}')
     print(f'number of available cores = {os.cpu_count()}')
     # Use input arguments for dataloader to get raw audio batches
     # must return in batches with asynchronous iterator to split batch into chunks - create class AudioIterator
@@ -40,16 +86,17 @@ async def main(in_args):
                   (23, 24), (25, 26), (27, 28), (29, 30), (31, 32), (33, 34), (35, 36), (37, 38), (39, 40), (41, 42),
                   (43, 44), (45, 46), (47, 48)]
     '''
-    audio_path = "C:\\Users\\estin\\PycharmProjects\\ElephantsDetector\\test_EC\\data\\nn09a_20201025_120105.wav"
-    target_sampling_rate = 8000  # the input audio will be resampled at this sampling rate 44100
-    n_channels = 1  # either 1 or 2
-    waveform_duration = 100  # in seconds 30
-    offset = 100  # this means that we read only in the interval [15s, duration of file] 15
-    dataloader = miniaudio.wav_stream_file(
-        filename=audio_path,
-        seek_frame=0,  # seek_frame = int(offset * target_sampling_rate),
-        frames_to_read=int(waveform_duration * target_sampling_rate)
-    )
+    data, fs = sf.read(AUDIO_FILE_PATH, always_2d=True)
+    print(f'sampling rate = {fs}')
+    processor_chunk_size = ceil(len(data) / os.cpu_count())
+    dataloader = []
+    current_chunk_position = 0
+    while current_chunk_position < len(data):
+        print(f'current_chunk_position = {current_chunk_position} is << len(data) {len(data)}')
+        current_chunk = data[current_chunk_position:current_chunk_position + processor_chunk_size]
+        dataloader.append(current_chunk)
+        current_chunk_position += processor_chunk_size
+
     final_results = []
     # create a pool with the number of available cores and distribute batches to separate processes
     async with Pool() as pool:
@@ -63,8 +110,13 @@ async def main(in_args):
 
     return final_results
 
+
 if __name__ == '__main__':
     data_dir = 'test_data_dir'
-    final = asyncio.run(main(data_dir))
-    print(f'final in __main__ = {final}')
+    try:
+        final = asyncio.run(main(data_dir))
+        print(f'final in __main__ = {final}')
+    except KeyboardInterrupt:
+        sys.exit('\nInterrupted by user')
+
 
