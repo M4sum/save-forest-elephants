423bf27f3ed6e7d883df67fb2cf1b926929cd7cf
EstineClasen <estineclasen@gmail.com>
2023-04-29 23:30:47 +0200

    Step2 with parallelizing spectrogram gen

--- temp_spectrogramer.py
+++ temp_spectrogramer.py
@@ -11,6 +11,7 @@ import resampy
 import soundfile as sf
 from multiprocessing import Pool, cpu_count
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+from matplotlib import mlab as ml
 
 TARGET_SR = 4000
 
@@ -22,7 +23,16 @@ Peter's new data! Assumes we only want to process .wav files.
 Let us also create a file at the end with all of the specs createdddd
 """
 
-def audio
+def audio_slice_to_spectogram(audio_chunk, idx, nfft, sr, noverlap, pad, max_f):
+    [spectrum_chunk, freqs_chunk, t_chunk] = ml.specgram(audio_chunk,
+                                             NFFT=nfft, Fs=sr, noverlap=noverlap,
+                                             window=ml.window_hanning, pad_to=pad)
+
+    # Cutout the high frequencies that are not of interest
+    spectrum_chunk = spectrum_chunk[(freqs_chunk <= max_f)]
+
+    return spectrum_chunk, idx
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -84,8 +94,9 @@ if __name__ == '__main__':
 
                 # We need to read the audio file so that we can use generate_spectrogram
                 audio_path = path.join(dirpath, audio_file)         # TODO TODO TODO NB!!!
-                audio_path = "input_data\\split\\dz_20120206_000000.wav"          # 8kHz file
-                # audio_path = "input_data\\split\\kp16_20150416_000000.wav"            # 4kHz file
+                audio_path = "input_data\\split\\dz_20120206_000000.wav"          # 8kHz 24h file
+                # audio_path = "input_data\\split\\kp16_20150416_000000.wav"            # 4kHz 24h file
+                # audio_path = "input_data\\split\\tree3_20180517_095900.wav"          # 14h file
                 try:
                     time_start_load_file = time()
                     raw_audio, samplerate = librosa.load(audio_path, sr=TARGET_SR, mono=True)
@@ -110,81 +121,56 @@ if __name__ == '__main__':
                 start_chunk = 0
                 audio_concat_chunks_for_spectogram = None
                 i = 0
-                final_spec = None
 
                 # divide the audio into equal parts, each part will be processed in a separate process
                 # Remember that we want to start as if we are doing one continuous sliding window
                 # So first generate concatenated array to process easier
-                # Can this while be done as part of the for loop below?  
+                # Can this while be done as part of the for loop below?
+                print(f'raw_audio.shape[0] = {raw_audio.shape[0]}')
                 while start_chunk + len_chunk < raw_audio.shape[0]:
                     if i == 0:
                         audio_concat_chunks_for_spectogram = raw_audio[start_chunk:start_chunk+len_chunk]
-                        i += 1
                     else:
                         audio_concat_chunks_for_spectogram = np.concatenate((audio_concat_chunks_for_spectogram,
                                                                              raw_audio[start_chunk:start_chunk +
                                                                                                    len_chunk]), axis=0)
+                    print(f'{i}th chunk concat loop')
+                    i += 1
                     start_chunk += len_chunk - NFFT + hop
                 # Do one final chunk for whatever remains at the end
                 audio_concat_chunks_for_spectogram = np.concatenate((audio_concat_chunks_for_spectogram,
                                                                      raw_audio[start_chunk:]), axis=0)
 
+                chunk_spec_dim = 154
+                final_spec = np.zeros(((i+1)*chunk_spec_dim, 1000))      # TODO fix this shape i*whatever gets returned per chunk of spect # spect_chunk.shape = (154,1000)
                 processes = []
                 start_chunk = 0
                 i = 0
                 with ProcessPoolExecutor() as executor:
                     for audio_idx in range(0, audio_concat_chunks_for_spectogram.shape[0], len_chunk):
-                        print(f'Chunk number {audio_idx}: data_id')
+                        print(f'Chunk number {audio_idx}: {data_id}')
                         # pdb.set_trace()
-                        audio_slice = audio_concat_chunks_for_spectogram[start_chunk:, start_chunk + len_chunk]
+                        audio_slice = audio_concat_chunks_for_spectogram[start_chunk: start_chunk + len_chunk]
                         start_chunk += len_chunk - NFFT + hop
-                        processes.append(executor.submit(process_slice, spect_slice, model_0,
-                                                         model_1, hierarchy_threshold,
-                                                         spect_idx, last_chunk_flag,
-                                                         predictions))
+                        processes.append(executor.submit(audio_slice_to_spectogram, audio_slice, audio_idx,
+                                                         NFFT, samplerate, NFFT-hop, pad_to, max_freq))
+
+                    # wait for all processes to finish and collect spectrogram chunks
+                    for process in as_completed(processes):
+                        # pdb.set_trace()
+                        spect_chunk, spect_idx = process.result()
+                        idx_number = int(spect_idx/len_chunk)
+                        print(f'size of spect_chunk = {spect_chunk.shape[0]}')
+                        final_spec[idx_number*chunk_spec_dim: (idx_number+1)*chunk_spec_dim, :] += spect_chunk
 
 
-
-
-                while start_chunk + len_chunk < raw_audio.shape[0]:
-                    if (i % 100 == 0):
-                        print("Chunk number " + str(i) + ": " + id)
-                    [spectrum, freqs, t] = ml.specgram(raw_audio[start_chunk: start_chunk + len_chunk],
-                                                       NFFT=NFFT, Fs=samplerate, noverlap=(NFFT - hop),
-                                                       window=ml.window_hanning, pad_to=pad_to)
-                    # Cutout the high frequencies that are not of interest
-                    spectrum = spectrum[(freqs <= max_freq)]
-
-                    if i == 0:
-                        final_spec = spectrum
-                    else:
-                        final_spec = np.concatenate((final_spec, spectrum), axis=1)
-
-                    # Remember that we want to start as if we are doing one continuous sliding window
-                    start_chunk += len_chunk - NFFT + hop
-                    i += 1
-
-                # Do one final chunk for whatever remains at the end
-                [spectrum, freqs, t] = ml.specgram(raw_audio[start_chunk: start_chunk + len_chunk],
-                                                   NFFT=NFFT, Fs=samplerate, noverlap=(NFFT - hop),
-                                                   window=ml.window_hanning, pad_to=pad_to)
-                # Cutout the high frequencies that are not of interest
-                spectrum = spectrum[(freqs <= max_freq)]
-                final_spec = np.concatenate((final_spec, spectrum), axis=1)
+                final_spec = final_spec.T
 
                 print("Finished making one 24 hour spectogram")
-                #return final_spec.T
-
-
-
-
-
-
-
 
                 # Want to save the corresponding label_file with the spectrogram!!
-                np.save(path.join(spect_dir, data_id + "_spec.npy"), spectrogram)
-                print ("processed " + data_id)
+                np.save(path.join(spect_dir, data_id + "_spec.npy"), final_spec)
+                print("processed " + data_id)
                 spect_files.append(data_id)
 
         # Save the spect ids
