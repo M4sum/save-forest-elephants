040de0a6242c2c2245b35c4be2f88e35854c1bfd
m4sum <32421365+M4sum@users.noreply.github.com>
2023-05-19 10:26:10 -0500

    remove helper and temp_spectrogrammer

--- helper.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import numpy as np
-import os
-
-def load_spectrograms(filename, file_id):
-    with open(filename, 'rb') as f:
-        spectrogram = np.load(f)
-    return spectrogram, file_id
-
